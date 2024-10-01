1c6fc3f852c04b6ece5bd1859ff975adad031d5e
A00299577 <A00299577@mycambrian.ca>
2024-10-01 19:58:00 +0000

    created a Todolist API

---
+++ TodoList/Models/ToDoItem.cs
@@ -0,0 +1,12 @@
+namespace TodoList.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime? DueDate { get; set; }
+        public DateTime? CompletedDate { get; set; }
+
+    }
+}
